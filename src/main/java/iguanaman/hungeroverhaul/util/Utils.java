105b0293f2b1a828db58e57bd6f80879928df99c
Progwml6 <progwml6@gmail.com>
2014-05-13 22:25:42 -0400

    add some AT entries, and code to replace the entries in the registry

--- src/main/java/iguanaman/hungeroverhaul/util/Utils.java
+++ src/main/java/iguanaman/hungeroverhaul/util/Utils.java
@@ -1,15 +1,38 @@
 package iguanaman.hungeroverhaul.util;
 
+import com.google.common.collect.BiMap;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.util.RegistryNamespaced;
 
 public class Utils
 {
 
     public static void replace (Block current, Block newBlock)
     {
+        overwriteEntry(Block.blockRegistry, current.getUnlocalizedName(), newBlock);
     }
+
     public static void replace (Item current, Item newItem)
     {
+        overwriteEntry(Item.itemRegistry, current.getUnlocalizedName(), newItem);
+
+    }
+
+    /*
+        @Author Skyboy & Team CoFH
+        used to replace entries in the registry
+    */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static void overwriteEntry (RegistryNamespaced registry, String name, Object object)
+    {
+
+        Object oldThing = registry.getObject(name);
+        int id = registry.getIDForObject(oldThing);
+        BiMap map = ((BiMap) registry.registryObjects);
+        registry.underlyingIntegerMap.func_148746_a(object, id);
+        map.remove(name);
+        map.forcePut(name, object);
     }
 }
