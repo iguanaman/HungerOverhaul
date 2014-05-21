6dacd53b2997baf4cac1b6f89456c9ae4c01c7d1
Parker Young <pyoung1998@gmail.com>
2014-05-20 22:59:05 -0400

    ModuleHarvestCraftTrees fixed

--- src/main/java/iguanaman/hungeroverhaul/module/ModuleHarvestCraftTrees.java
+++ src/main/java/iguanaman/hungeroverhaul/module/ModuleHarvestCraftTrees.java
@@ -2,6 +2,7 @@ package iguanaman.hungeroverhaul.module;
 
 import iguanaman.hungeroverhaul.blocks.IguanaFruit;
 import iguanaman.hungeroverhaul.util.Utils;
+import net.minecraft.block.Block;
 
 import com.pam.harvestcraft.BlockRegistry;
 
@@ -9,50 +10,94 @@ public class ModuleHarvestCraftTrees {
 
 	public static void init()
 	{
-		BlockRegistry.pamApple = new IguanaFruit("apple");
-		BlockRegistry.pamAvocado = new IguanaFruit("avocado");
-		BlockRegistry.pamBanana = new IguanaFruit("banana");
-		BlockRegistry.pamCherry = new IguanaFruit("cherry");
-		BlockRegistry.pamCinnamon = new IguanaFruit("cinnamon");
-		BlockRegistry.pamCoconut = new IguanaFruit("coconut");
-		BlockRegistry.pamDragonfruit = new IguanaFruit("dragonfruit");
-		BlockRegistry.pamLemon = new IguanaFruit("lemon");
-		BlockRegistry.pamLime = new IguanaFruit("lime");
-		BlockRegistry.pamMango = new IguanaFruit("mango");
-		BlockRegistry.pamNutmeg = new IguanaFruit("nutmeg");
-		BlockRegistry.pamOlive = new IguanaFruit("olive");
-		BlockRegistry.pamOrange = new IguanaFruit("orange");
-		BlockRegistry.pamPapaya = new IguanaFruit("papaya");
-		BlockRegistry.pamPeach = new IguanaFruit("peach");
-		BlockRegistry.pamPear = new IguanaFruit("pear");
-		BlockRegistry.pamPeppercorn = new IguanaFruit("peppercorn");
-		BlockRegistry.pamPlum = new IguanaFruit("plum");
-		BlockRegistry.pamPomegranate = new IguanaFruit("pomegranate");
-		BlockRegistry.pamStarfruit = new IguanaFruit("starfruit");
-		BlockRegistry.pamVanillabean = new IguanaFruit("vanillabean");
-		BlockRegistry.pamWalnut = new IguanaFruit("walnut");
+		String tempName;
 		
-		Utils.replace(BlockRegistry.pamApple, new IguanaFruit("apple"));
-		Utils.replace(BlockRegistry.pamAvocado, new IguanaFruit("avocado"));
-		Utils.replace(BlockRegistry.pamBanana, new IguanaFruit("banana"));
-		Utils.replace(BlockRegistry.pamCherry, new IguanaFruit("cherry"));
-		Utils.replace(BlockRegistry.pamCinnamon, new IguanaFruit("cinnamon"));
-		Utils.replace(BlockRegistry.pamCoconut, new IguanaFruit("coconut"));
-		Utils.replace(BlockRegistry.pamDragonfruit, new IguanaFruit("dragonfruit"));
-		Utils.replace(BlockRegistry.pamLemon, new IguanaFruit("lemon"));
-		Utils.replace(BlockRegistry.pamLime, new IguanaFruit("lime"));
-		Utils.replace(BlockRegistry.pamMango, new IguanaFruit("mango"));
-		Utils.replace(BlockRegistry.pamNutmeg, new IguanaFruit("nutmeg"));
-		Utils.replace(BlockRegistry.pamOlive, new IguanaFruit("olive"));
-		Utils.replace(BlockRegistry.pamOrange, new IguanaFruit("orange"));
-		Utils.replace(BlockRegistry.pamPapaya, new IguanaFruit("papaya"));
-		Utils.replace(BlockRegistry.pamPeach, new IguanaFruit("peach"));
-		Utils.replace(BlockRegistry.pamPear, new IguanaFruit("pear"));
-		Utils.replace(BlockRegistry.pamPeppercorn, new IguanaFruit("peppercorn"));
-		Utils.replace(BlockRegistry.pamPlum, new IguanaFruit("plum"));
-		Utils.replace(BlockRegistry.pamPomegranate, new IguanaFruit("pomegranate"));
-		Utils.replace(BlockRegistry.pamStarfruit, new IguanaFruit("starfruit"));
-		Utils.replace(BlockRegistry.pamVanillabean, new IguanaFruit("vanillabean"));
-		Utils.replace(BlockRegistry.pamWalnut, new IguanaFruit("walnut"));
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamApple);
+		BlockRegistry.pamApple = new IguanaFruit("apple");
+		Utils.replace(BlockRegistry.pamApple, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamAvocado);
+		BlockRegistry.pamAvocado = new IguanaFruit("avocado");
+		Utils.replace(BlockRegistry.pamAvocado, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamBanana);
+		BlockRegistry.pamBanana = new IguanaFruit("banana");
+		Utils.replace(BlockRegistry.pamBanana, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamCherry);
+		BlockRegistry.pamCherry = new IguanaFruit("cherry");
+		Utils.replace(BlockRegistry.pamCherry, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamCinnamon);
+		BlockRegistry.pamCinnamon = new IguanaFruit("cinnamon");
+		Utils.replace(BlockRegistry.pamCinnamon, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamCoconut);
+		BlockRegistry.pamCoconut = new IguanaFruit("coconut");
+		Utils.replace(BlockRegistry.pamCoconut, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamDragonfruit);
+		BlockRegistry.pamDragonfruit = new IguanaFruit("dragonfruit");
+		Utils.replace(BlockRegistry.pamDragonfruit, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamLemon);
+		BlockRegistry.pamLemon = new IguanaFruit("lemon");
+		Utils.replace(BlockRegistry.pamLemon, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamLime);
+		BlockRegistry.pamLime = new IguanaFruit("lime");
+		Utils.replace(BlockRegistry.pamLime, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamMango);
+		BlockRegistry.pamMango = new IguanaFruit("mango");
+		Utils.replace(BlockRegistry.pamMango, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamNutmeg);
+		BlockRegistry.pamNutmeg = new IguanaFruit("nutmeg");
+		Utils.replace(BlockRegistry.pamNutmeg, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamOlive);
+		BlockRegistry.pamOlive = new IguanaFruit("olive");
+		Utils.replace(BlockRegistry.pamOlive, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamOrange);
+		BlockRegistry.pamOrange = new IguanaFruit("orange");
+		Utils.replace(BlockRegistry.pamOrange, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamPapaya);
+		BlockRegistry.pamPapaya = new IguanaFruit("papaya");
+		Utils.replace(BlockRegistry.pamPapaya, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamPeach);
+		BlockRegistry.pamPeach = new IguanaFruit("peach");
+		Utils.replace(BlockRegistry.pamPeach, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamPear);
+		BlockRegistry.pamPear = new IguanaFruit("pear");
+		Utils.replace(BlockRegistry.pamPear, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamPeppercorn);
+		BlockRegistry.pamPeppercorn = new IguanaFruit("peppercorn");
+		Utils.replace(BlockRegistry.pamPeppercorn, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamPlum);
+		BlockRegistry.pamPlum = new IguanaFruit("plum");
+		Utils.replace(BlockRegistry.pamPlum, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamPomegranate);
+		BlockRegistry.pamPomegranate = new IguanaFruit("pomegranate");
+		Utils.replace(BlockRegistry.pamPomegranate, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamStarfruit);
+		BlockRegistry.pamStarfruit = new IguanaFruit("starfruit");
+		Utils.replace(BlockRegistry.pamStarfruit, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamVanillabean);
+		BlockRegistry.pamVanillabean = new IguanaFruit("vanillabean");
+		Utils.replace(BlockRegistry.pamVanillabean, tempName);
+		
+		tempName = Block.blockRegistry.getNameForObject(BlockRegistry.pamWalnut);
+		BlockRegistry.pamWalnut = new IguanaFruit("walnut");
+		Utils.replace(BlockRegistry.pamWalnut, tempName);
 	}
 }
