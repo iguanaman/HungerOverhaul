fe47972f5014ac78031c904919950112e5aa13f0
Parker <pyoung1998@gmail.com>
2014-07-16 15:38:15 -0400

    Some small command work Added tab auto-complete for username, removed unnecessary method wrapper, and some other small tweaks.

--- src/main/java/iguanaman/hungeroverhaul/commands/IguanaCommandHunger.java
+++ src/main/java/iguanaman/hungeroverhaul/commands/IguanaCommandHunger.java
@@ -1,9 +1,12 @@
 package iguanaman.hungeroverhaul.commands;
 
+import java.util.List;
+
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 
 public class IguanaCommandHunger extends CommandBase {
 
@@ -26,28 +29,22 @@ public class IguanaCommandHunger extends CommandBase {
 		if (astring.length > 0)
 		{
 			EntityPlayerMP entityplayermp = astring.length >= 2 ? getPlayer(icommandsender, astring[0]) : getCommandSenderAsPlayer(icommandsender);
-			int foodValue = astring.length >= 2 ? parseIntWithMinMax(icommandsender, astring[1], 0, 20) : parseIntWithMinMax(icommandsender, astring[0], 0, 20);
+			int foodValue = astring.length >= 2 ? parseIntBounded(icommandsender, astring[1], 0, 20) : parseIntBounded(icommandsender, astring[0], 0, 20);
 			entityplayermp.getFoodStats().setFoodLevel(foodValue);
-			func_152374_a(icommandsender, this, 1, "Set " + icommandsender.getCommandSenderName() + "'s hunger to " + foodValue, new Object[0]);
+			func_152374_a(icommandsender, this, 1, "Set " + icommandsender.getCommandSenderName() + "'s hunger to " + foodValue);
 		} else {
-			throw new WrongUsageException("hunger [player] <value>", new Object[0]);
+			throw new WrongUsageException("/hunger [player] <value>");
 		}
 
 	}
 
-
-	/**
-	 * Parses an int from the given sring with a specified minimum.
-	 */
-	public static int parseIntWithMinMax(ICommandSender par0ICommandSender, String par1Str, int min, int max)
-	{
-		return parseIntBounded(par0ICommandSender, par1Str, min, max);
-	}
-
 	@Override
 	public String getCommandUsage(ICommandSender icommandsender) {
-		// TODO Auto-generated method stub
-		return null;
+		return "/hunger [player] <value>";
 	}
 
+    @Override
+    public List addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_) {
+        return p_71516_2_.length == 1 ? getListOfStringsMatchingLastWord(p_71516_2_, MinecraftServer.getServer().getAllUsernames()) : null;
+    }
 }
