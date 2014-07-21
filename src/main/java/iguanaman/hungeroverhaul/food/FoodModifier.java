322f3e52cdea56142208a583b8668519bddfaf86
squeek <squeek502@hotmail.com>
2014-07-20 21:13:45 -0700

    Moved modified food value registry to FoodModifer

--- src/main/java/iguanaman/hungeroverhaul/food/FoodModifier.java
+++ src/main/java/iguanaman/hungeroverhaul/food/FoodModifier.java
@@ -3,11 +3,16 @@ package iguanaman.hungeroverhaul.food;
 import iguanaman.hungeroverhaul.IguanaConfig;
 import iguanaman.hungeroverhaul.api.FoodValues;
 import iguanaman.hungeroverhaul.api.IDefaultFoodValueModifier;
-import iguanaman.hungeroverhaul.helper.FoodValueHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class FoodModifier implements IDefaultFoodValueModifier
 {
+    private static HashMap<ItemStack, FoodValues> modifiedFoodValues = new HashMap<ItemStack, FoodValues>();
 
     @Override
     public int getPriority()
@@ -19,7 +24,7 @@ public class FoodModifier implements IDefaultFoodValueModifier
     @Override
     public FoodValues getModifiedFoodValues(FoodValues foodValues, ItemStack itemStack)
     {
-        FoodValues modifiedFoodValues = FoodValueHelper.getModifiedFoodValues(itemStack);
+        FoodValues modifiedFoodValues = lookupModifiedFoodValues(itemStack);
         if (modifiedFoodValues != null)
             return modifiedFoodValues;
         else
@@ -28,6 +33,28 @@ public class FoodModifier implements IDefaultFoodValueModifier
             float saturationValue = Math.max(foodValue / 20F, 0F);
             return new FoodValues(foodValue, saturationValue);
         }
+    }
+
+    public static void setModifiedFoodValues(Item item, FoodValues values)
+    {
+        modifiedFoodValues.put(new ItemStack(item), values);
+    }
+
+    public static void setModifiedFoodValues(ItemStack stack, FoodValues values)
+    {
+        modifiedFoodValues.put(stack, values);
+    }
+
+    private static FoodValues lookupModifiedFoodValues(ItemStack stack)
+    {
+        for (Map.Entry<ItemStack, FoodValues> entry : modifiedFoodValues.entrySet())
+        {
+            if (stack.isItemEqual(entry.getKey()))
+            {
+                return entry.getValue();
+            }
+        }
+        return null;
     }
 
 }
--- src/main/java/iguanaman/hungeroverhaul/module/ModuleVanilla.java
+++ src/main/java/iguanaman/hungeroverhaul/module/ModuleVanilla.java
@@ -2,7 +2,7 @@ package iguanaman.hungeroverhaul.module;
 
 import iguanaman.hungeroverhaul.IguanaConfig;
 import iguanaman.hungeroverhaul.api.FoodValues;
-import iguanaman.hungeroverhaul.helper.FoodValueHelper;
+import iguanaman.hungeroverhaul.food.FoodModifier;
 import net.minecraft.init.Items;
 import cpw.mods.fml.common.Loader;
 
@@ -12,25 +12,25 @@ public class ModuleVanilla
     {
         if (Loader.isModLoaded("harvestcraft") || IguanaConfig.modifyFoodValues)
         {
-            FoodValueHelper.addModifiedFoodValues(Items.apple, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.bread, new FoodValues(3, 0.2F));
-            FoodValueHelper.addModifiedFoodValues(Items.porkchop, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.cooked_porkchop, new FoodValues(2, 0.15F));
-            FoodValueHelper.addModifiedFoodValues(Items.fish, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.cooked_fished, new FoodValues(2, 0.1F));
-            FoodValueHelper.addModifiedFoodValues(Items.cookie, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.melon, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.beef, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.cooked_beef, new FoodValues(2, 0.15F));
-            FoodValueHelper.addModifiedFoodValues(Items.chicken, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.cooked_chicken, new FoodValues(2, 0.15F));
-            FoodValueHelper.addModifiedFoodValues(Items.rotten_flesh, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.baked_potato, new FoodValues(2, 0.15F));
-            FoodValueHelper.addModifiedFoodValues(Items.poisonous_potato, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.pumpkin_pie, new FoodValues(3, 0.15F));
-            FoodValueHelper.addModifiedFoodValues(Items.mushroom_stew, new FoodValues(2, 0.1F));
-            FoodValueHelper.addModifiedFoodValues(Items.carrot, new FoodValues(1, 0.05F));
-            FoodValueHelper.addModifiedFoodValues(Items.potato, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.apple, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.bread, new FoodValues(3, 0.2F));
+            FoodModifier.setModifiedFoodValues(Items.porkchop, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.cooked_porkchop, new FoodValues(2, 0.15F));
+            FoodModifier.setModifiedFoodValues(Items.fish, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.cooked_fished, new FoodValues(2, 0.1F));
+            FoodModifier.setModifiedFoodValues(Items.cookie, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.melon, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.beef, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.cooked_beef, new FoodValues(2, 0.15F));
+            FoodModifier.setModifiedFoodValues(Items.chicken, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.cooked_chicken, new FoodValues(2, 0.15F));
+            FoodModifier.setModifiedFoodValues(Items.rotten_flesh, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.baked_potato, new FoodValues(2, 0.15F));
+            FoodModifier.setModifiedFoodValues(Items.poisonous_potato, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.pumpkin_pie, new FoodValues(3, 0.15F));
+            FoodModifier.setModifiedFoodValues(Items.mushroom_stew, new FoodValues(2, 0.1F));
+            FoodModifier.setModifiedFoodValues(Items.carrot, new FoodValues(1, 0.05F));
+            FoodModifier.setModifiedFoodValues(Items.potato, new FoodValues(1, 0.05F));
         }
     }
 }
