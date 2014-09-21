d700293f6c941cff2a0b795a9e027a33851caf62
Parker Young <Parker8283@users.noreply.github.com>
2014-09-20 23:42:50 -0400

    Added BOP Foods

--- src/main/java/iguanaman/hungeroverhaul/module/ModuleBOP.java
+++ src/main/java/iguanaman/hungeroverhaul/module/ModuleBOP.java
@@ -1,9 +1,24 @@
 package iguanaman.hungeroverhaul.module;
 
+import net.minecraft.item.ItemStack;
+
+import biomesoplenty.api.content.BOPCItems;
+import iguanaman.hungeroverhaul.food.FoodModifier;
+import squeek.applecore.api.food.FoodValues;
+
 public class ModuleBOP
 {
     public static void init()
     {
-        //TODO add support for BOP foods here!
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 0), new FoodValues(1, 0.05F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 1), new FoodValues(1, 0.05F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 2), new FoodValues(1, 0.05F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 4), new FoodValues(3, 0.15F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 5), new FoodValues(3, 0.15F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 6), new FoodValues(3, 0.15F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 8), new FoodValues(1, 0.05F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 9), new FoodValues(1, 0.05F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 10), new FoodValues(3, 0.2F));
+        FoodModifier.setModifiedFoodValues(new ItemStack(BOPCItems.food, 1, 11), new FoodValues(1, 0.05F));
     }
 }
