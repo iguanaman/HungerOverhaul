45de95bc0a109e89fa47452e11ef8ad7047836b8
squeek <squeek502@hotmail.com>
2014-09-25 17:52:32 -0700

    Potentially safer way to access the player send queue

--- src/main/java/iguanaman/hungeroverhaul/util/ClientHelper.java
+++ src/main/java/iguanaman/hungeroverhaul/util/ClientHelper.java
@@ -1,6 +1,5 @@
 package iguanaman.hungeroverhaul.util;
 
-import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import cpw.mods.fml.client.FMLClientHandler;
@@ -12,6 +11,6 @@ public class ClientHelper
 {
     public static void sendRightClickPacket(int x, int y, int z, int side, ItemStack currentItem, float hitX, float hitY, float hitZ)
     {
-        ((NetHandlerPlayClient) FMLClientHandler.instance().getClientPlayHandler()).addToSendQueue(new C08PacketPlayerBlockPlacement(x, y, z, side, currentItem, hitX, hitY, hitZ));
+        FMLClientHandler.instance().getClientPlayerEntity().sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(x, y, z, side, currentItem, hitX, hitY, hitZ));
     }
 }
